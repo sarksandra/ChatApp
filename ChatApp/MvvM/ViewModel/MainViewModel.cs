fcd4361434e38145fe96dccf7fa7ede2c92747af
opilane <opilane@HP-CZC1339D2B>
2025-03-14 10:07:03 +0200

    Add project files.

---
+++ ChatApp/MvvM/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ChatApp.MvvM.Core;
+using ChatApp.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.MvvM.ViewModel
+{
+    class MainViewModel
+    {
+        public RelayCommand ConnectToServerCommand { get; set; }
+
+        public Server _server;
+
+        public MainViewModel()
+        {
+            _server = new Server();
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnecToServer());
+
+        }
+    }
+}
---
+++ ChatService/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ChatApp
+{
+    class Program
+    {
+        static TcpListener _listener;
+        static void Main(string[] args)
+        {
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
+            _listener.Start();
+
+            var client = _listener.AcceptTcpClient();
+            Console.WriteLine("Client has connected");
+        }
+    }
+}
