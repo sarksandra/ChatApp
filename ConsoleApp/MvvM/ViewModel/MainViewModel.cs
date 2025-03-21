4146c2676c8b894bda93da012287512386f3de00
opilane <opilane@HP-CZC1339D2P>
2025-03-21 11:28:07 +0200

    surmf

---
+++ ConsoleApp/MvvM/ViewModel/MainViewModel.cs
@@ -0,0 +1,58 @@
+﻿using ConsoleApp.Model;
+using ConsoleApp.Net;
+using System.Collections.ObjectModel;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp.MvvM.ViewModel
+{
+    class MainViewModel
+    {
+        public ObservableCollection<UserModel> Users { get; set; }
+        public ObservableCollection<string> Messages { get; set; }
+
+
+        public string Username { get; set; }
+
+        public string Message { get; set; }
+
+        public Server _server;
+
+        public MainViewModel()
+        {
+            Users = new ObservableCollection<UserModel>();
+            Messages = new ObservableCollection<string>();
+            _server = new Server();
+            _server.connectedEvent += UserConnected;
+            _server.msgRecivedEvent += MessageRecived;
+            _server.disconnect += RemoveUser;
+           
+        }
+
+        private void UserConnected()
+        {
+            var user = new UserModel
+            {
+                UserName = _server.PacketReader.ReadMessage(),
+                UID = _server.PacketReader.ReadMessage(),
+            };
+            if (!Users.Any(x => x.UID == user.UID))
+            {
+                Users.Add(user);
+
+
+            }
+        }
+        private void MessageRecived()
+        {
+            var msg = _server.PacketReader.ReadMessage();
+           Messages.Add(msg);
+
+        }
+        private void RemoveUser()
+        {
+            var uid = _server.PacketReader.ReadMessage();
+            var user = Users.Where(x => x.UID == uid).FirstOrDefault();
+            Users.Remove(user);
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp.MvvM.ViewModel;
+using ConsoleApp.Net;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            MainViewModel mainViewModel = new MainViewModel();
+            mainViewModel._server.ConnecToServer("tervist");
+            mainViewModel._server.SendMessageToServer("tere");
+
+
+
+
+
+        }
+        
+    }
+    
+}
